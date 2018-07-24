e700d6cc945ef5a862a04d31fd6277a52a9de963
cain910772 <cain910772@yahoo.com>
2018-07-24 09:04:33 -0500

    first

---
+++ main.js
@@ -0,0 +1,20 @@
+
+
+let bandId = 1
+
+
+
+
+
+const numberAssigned = function(bandName){
+    bandList = bandId + "."+bandName;
+    bandId += 1;
+    return bandList;
+        
+    }
+
+const getTheLedOut = numberAssigned("led Zeppelin")
+console.log(getTheLedOut);
+const opiate = numberAssigned("Tool");
+console.log(opiate);
+
